7652dba5fc6ddf3c9b1593db42cbb25208a63a09
na-ji <naji.demolitionman@gmail.com>
2014-07-30 15:32:02 +0200

    Configure Sonata Formatter

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -37,6 +37,7 @@ class AppKernel extends Kernel
             new JuniorEsiee\PageBundle\JuniorEsieePageBundle(),
             new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
             new Application\Sonata\BlockBundle\ApplicationSonataBlockBundle(),
+            new Application\Sonata\AdminBundle\ApplicationSonataAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
